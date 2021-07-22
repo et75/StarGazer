6f05e93b463e710842aa62e95aa4585629087edf
Emad Tirmizi <43972987+et75@users.noreply.github.com>
2021-07-22 22:27:53 +0000

    Added Ability to upload image to Discord and Delete the File from Host Server

--- main.py
+++ main.py
@@ -5,28 +5,13 @@ import json
 from PIL import Image
 import urllib.request
 
+#  img = Image.open(file_name)
 
-response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-
-obj = json.loads(response.text)
-
-
-url = obj['hdurl']
-file_name = url.split("/")[-1:][0]
-
-print(file_name)
-
-urllib.request.urlretrieve(url, file_name)
+#  img.show()
 
 
 
-img = Image.open(file_name)
 
-img.show()
-
-os.remove(file_name)
-
-''''
 client = discord.Client()
 
 @client.event
@@ -38,8 +23,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('~hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('~POTD'):
+        await message.channel.send('Heres the Picture of the Day!')
+        response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        obj = json.loads(response.text)
+        url = obj['hdurl']
+        file_name = url.split("/")[-1:][0]
+        print(file_name)
+        urllib.request.urlretrieve(url, file_name)
+        await message.channel.send(file=discord.File(file_name))
+        os.remove(file_name)
+        
 
 client.run(os.getenv('TOKEN'))
+
+
+
+'''
+~POTD
+  - default to today
+~POTD YYYY-MM-DD
+~Random #
+~MarsPOTD
+  - Latest Date
+~MarsPOTD YYYY-MM-DD
+~
 '''
